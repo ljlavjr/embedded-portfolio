fe90da1794812c119ab92055a0f5e10e3d636c7d
Larry <l5lavigne01@gmail.com>
2026-01-28 21:38:33 -0600

    02-uart-driver: debugging transmit

--- 02-uart-driver/main.c
+++ 02-uart-driver/main.c
@@ -2,17 +2,17 @@
 #include <avr/io.h>
 
 int main(void) {
-    DDRB |= (1 << PB7);  // LED as output
     
     uart_init(9600);
     uart_print("Hello from ATmega2560!\r\n");
     uart_print("Type something:\r\n");
     
-    while (1) {
-        char c = uart_receive();
-        uart_transmit(c);
-        PORTB ^= (1 << PB7);  // toggle LED on each character
-    }
+   while (1) {
+    char c = uart_receive();
+    uart_print("Got: ");
+    uart_transmit(c);
+    uart_print("\r\n");
+}
     
     return 0;
 }
