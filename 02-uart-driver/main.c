ddfbfe960dc48df53ce362b49cf9b6340fe29c9b
Larry <l5lavigne01@gmail.com>
2026-01-28 21:31:22 -0600

    02-uart-driver: add echo functionality

--- 02-uart-driver/main.c
+++ 02-uart-driver/main.c
@@ -3,8 +3,11 @@
 int main(void) {
     uart_init(9600);
     uart_print("Hello from ATmega2560!\r\n");
+    uart_print("Type something:\r\n");
+    while (1) {
+        char c = uart_receive();
+        uart_transmit(c);
+    }
 
-    while (1) {}
-    
     return 0;
 } 
