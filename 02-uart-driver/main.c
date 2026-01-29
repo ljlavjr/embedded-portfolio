8ce63bc43940cecdaa2abbc6cd943125f7178a23
Larry <l5lavigne01@gmail.com>
2026-01-28 21:35:14 -0600

    02-uart-driver: debugging echo functionality

--- 02-uart-driver/main.c
+++ 02-uart-driver/main.c
@@ -1,13 +1,18 @@
 #include "uart.h"
+#include <avr/io.h>
 
 int main(void) {
+    DDRB |= (1 << PB7);  // LED as output
+    
     uart_init(9600);
     uart_print("Hello from ATmega2560!\r\n");
     uart_print("Type something:\r\n");
+    
     while (1) {
         char c = uart_receive();
         uart_transmit(c);
+        PORTB ^= (1 << PB7);  // toggle LED on each character
     }
-
+    
     return 0;
-} 
+}
