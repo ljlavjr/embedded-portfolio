01f3a041cd45a1544ebef621220cb97039e6f9e2
Larry Lavigne <larry.lavigne@mycardmarket.com>
2026-02-16 15:54:57 -0600

    11-real-time-sensor-system: Added and updated adc driver code. Created tasks outlines and implemented the fast and slow sensor tasks

---
+++ 11-real-time-sensor-system/include/adc.h
@@ -0,0 +1,16 @@
+#ifndef ADC_H
+#define ADC_H
+
+#include "stm32f407.h"
+#include <stdint.h>
+
+
+#define ADON 0
+#define SWSTART 30
+
+void adc_init(void);
+void adc_init_dma(uint8_t channel);
+void adc_start(void);
+uint16_t adc_read(uint8_t channel);
+
+#endif
--- 11-real-time-sensor-system/src/main.c
+++ 11-real-time-sensor-system/src/main.c
@@ -10,15 +10,23 @@
 #include "timing.h"
 #include "uart.h"
 #include "queue.h"
+#include "adc.h"
 
 /* ---------------- Defines ----------------- */
 #define STACK_SIZE  256
+
+typedef struct {
+    uint8_t  channel;
+    uint16_t raw_value;
+    uint16_t timestamp;     // tick count when sampled
+} SensorReading_t;
 
 /* ------------ Global Handles -------------- */
 static TaskTiming_t fast_sensor;
 static TaskTiming_t slow_sensor;
 static TaskTiming_t processing;
 static TaskTiming_t reporter;
+QueueHandle_t queue;
 
 /* ----------- Task Prototypes -------------- */
 void vTaskFastSensor(void *pvParameters);
@@ -33,6 +41,9 @@ int main(void)
 {
     /* ---- Hardware Init ---- */
     timing_init();
+
+    /* ---- Create Queue ---- */
+    queue = xQueueCreate(10, sizeof(SensorReading_t));
 
     /* ---- Create Tasks ---- */
     xTaskCreate(
@@ -82,11 +93,16 @@ void vTaskFastSensor(void *pvParameters) {
     (void)pvParameters;
     TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xPeriod = pdMS_TO_TICKS(100);
+    SensorReading_t sensorReading;
 
     for (;;) {
         timing_jitter(&fast_sensor, xLastWakeTime + xPeriod);
         timing_start(&fast_sensor);
-        // Work
+        uint16_t raw_value = adc_read(0);
+        sensorReading.channel = 0;
+        sensorReading.raw_value = raw_value;
+        sensorReading.timestamp = xTaskGetTickCount();
+        xQueueSend(queue, &sensorReading, 0);
         timing_stop(&fast_sensor);
         vTaskDelayUntil(&xLastWakeTime, xPeriod);
     }
@@ -96,11 +112,16 @@ void vTaskSlowSensor(void *pvParameters) {
     (void)pvParameters;
     TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xPeriod = pdMS_TO_TICKS(500);
+    SensorReading_t sensorReading;
 
     for (;;) {
         timing_jitter(&slow_sensor, xLastWakeTime + xPeriod);
         timing_start(&slow_sensor);
-        // Work
+        uint16_t raw_value = adc_read(16);
+        sensorReading.channel = 16;
+        sensorReading.raw_value = raw_value;
+        sensorReading.timestamp = xTaskGetTickCount();
+        xQueueSend(queue, &sensorReading, 0);
         timing_stop(&slow_sensor);
         vTaskDelayUntil(&xLastWakeTime, xPeriod);
     }
